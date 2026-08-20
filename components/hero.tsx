85659b627a71829589abc01de139df511d008877
Oliver Götz <goetzoliver89@gmail.com>
2026-08-20 14:40:34 +0200

    fix: mobile presentation

--- components/hero.tsx
+++ components/hero.tsx
@@ -8,49 +8,6 @@ import { ChevronDown } from "lucide-react";
 export const Hero = () => {
   return (
     <div className="w-full  md:py-5  bg-transparent">
-      {/* <div className="relative min-h-screen text-white ">
-        <div className="relative text-white inset-0 animate-ken-burns  bg-blend-overlay bg-slate-200/30 min-h-screen bg-center z-10 bg-cover bg-[url(/novotec_Foto_drei_Transporter.jpg)]">
-        <h2 className="text-2xl uppercase  text-right font-semibold mb-10 border-b pb-2">
-          <NovoTec /> :: Wofür stehen wir?
-        </h2>
-
-        <Image
-          src="/novotec_Foto_drei_Transporter.jpg"
-          alt=""
-          fill
-          priority
-          className="object-cover object-center  inset-0 animate-ken-burns -z-20"
-        />
-        <div className="absolute inset-0 bg-slate-200/20 animate-ken-burns " />
-        <div className="min-h-screen z-20 container flex items-center justify-center">
-          <div className="grid md:grid-cols-2 max-w-[1000px] gap-4 text-sm text-center">
-            <div className={box_style}>
-              <div>
-                <ThumbsUp className="text-primary-foreground" size={50} />
-              </div>
-              <h3 className="font-semibold text-lg text-white">
-                Persönlich und professionell
-              </h3>
-              <p>
-                Persönliche und professionelle Immobiliensanierungen und
-                -ausbauten im Raum Köln und ganz NRW aus einer Hand.
-              </p>
-            </div>
-            <div className={box_style}>
-              <div>
-                <Star className="text-primary-foreground" size={50} />
-              </div>
-              <h3 className="font-semibold text-lg  text-white">
-                Fachkompetenz
-              </h3>
-              <p>
-                Ihr Generalunternehmer mit Fachkompetenz in allen Gewerken für
-                erstklassige Ergebnisse.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div> */}
       <section className="">
         {/* <CrawlingText /> */}
         <div className="absolute w-full md:w-2/3 top-1/4 left-0 z-10 text-center md:text-left text-white font-bold">
@@ -89,12 +46,12 @@ export const Hero = () => {
         </div>
         <BackgroundVideo />
 
-        <div className="container py-20 font-semibold flex flex-wrap justify-center gap-7 mx-auto">
+        <div className="container py-20 font-semibold flex flex-col md:flex-wrap md:justify-center gap-7 mx-auto">
           {coreCompetencies.map((c) => (
             <Link
               key={c.title}
               href="/leistungen"
-              className="w-[calc(50%-14px)] md:w-[calc(25%-21px)]"
+              className="md:w-[calc(25%-21px)]"
             >
               <div className="h-[80px] bg-slate-100 flex items-center justify-center px-1  border rounded-md shadow-lg">
                 <div
