f230368260ebbacbecb17cae7af96830cddbeaec
Oliver Götz <goetzoliver89@gmail.com>
2026-09-12 19:44:26 +0200

    fix: hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -71,7 +71,7 @@ export const Hero = () => {
         </div>
 
         {/* Trust Badge & CTA */}
-        <div className="mb-12 flex flex-col  items-center gap-6 md:flex-row md:items-center md:gap-8">
+        <div className="mb-12 flex flex-col mt-24 md:mt-0 items-center gap-6 md:flex-row md:items-center md:gap-8">
           {/* Trust Badge */}
           <div className="flex items-center gap-4">
             <div className="flex h-20 w-20 flex-shrink-0 items-center justify-center rounded-full bg-transparent">
